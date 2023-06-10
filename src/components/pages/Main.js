e150601078c4c9ee787920904716d63c9cb44b9b
Maxime <sowlution.pro@gmail.com>
2023-06-10 15:46:07 +0200

    .

--- src/components/pages/Main.js
+++ src/components/pages/Main.js
@@ -6,9 +6,8 @@ function Main() {
     <div className="mainstyle-container">
       <h1>🌈 Discovery Wallet</h1>
       <div className="mainstyle-card">
-        <p>Discovery Wallet is an ambitious project aimed at providing users with a user-friendly and secure solution to initiate their first DeFi transactions. The wallet will be built on Web3 Blockchain technology, offering enhanced security, increased privacy, and an optimal user experience.
-        <br></br><br></br>📚By integrating ETH as the base fund, providing access to Aave for DeFi lending and borrowing, and utilizing Optimism's scaling solution, our wallet aims to make the DeFi experience more accessible and secure for all users with the use of the account abstraction (ERC-4337).</p>
-      </div>
+        <p>Discovery Wallet is an ambitious project aimed at providing users with a user-friendly and secure solution to initiate their first DeFi transactions. The wallet will be built using Smart Contract Wallet based on ERC4337 offering an optimal user experience.
+          </p></div>
     </div>
   );
 }
