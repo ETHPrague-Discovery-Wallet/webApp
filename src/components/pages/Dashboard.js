e6d4f8e7dd88393c48b56236a5152d1e04effe6e
Maxime <sowlution.pro@gmail.com>
2023-06-11 03:37:04 +0200

    add visualisation of authorized addresses

--- src/components/pages/Dashboard.js
+++ src/components/pages/Dashboard.js
@@ -1,18 +1,17 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useAccount } from 'wagmi'
 import "./styles/mainstyle.css"
 import abstractedABI from "../../abi/abstracted_abi.json"
-import factoryABI from "../../abi/factory_abi.json"
-import { useNetwork, useContractWrite,usePrepareContractWrite, useContractRead } from 'wagmi';
+import { useNetwork, useContractRead } from 'wagmi';
 import { useBalance } from 'wagmi'
-
+import contractsData from "./contractsData.json";
 
 function Dashboard() {
 
   const factoryAddress = "0x"
   const discoveryAccount = "0x"
 
-  const { address : userAddress, isConnecting, isDisconnected } = useAccount();
+  const { address: userAddress, isConnecting, isDisconnected } = useAccount();
   const { data: balance } = useBalance({
     address: userAddress,
   })
@@ -22,49 +21,47 @@ function Dashboard() {
   }, [userAddress, isConnecting, isDisconnected]);
 
   // READ CONTRACT
-  const { data : hasDeployed } = useContractRead({
+  const { data: authorizedAddress } = useContractRead({
     address: factoryAddress,
-    abi: factoryABI,
-    functionName: 'getDeployment',
-    args: [userAddress],
+    abi: abstractedABI,
+    functionName: 'getAllAuthorizedAddress',
   })
 
-  // WRITE CONTRACT
-  
-  // // Prepare contract for minting
-  // const { config : configMinting } = usePrepareContractWrite({
-  //     address: proxyAddress, 
-  //     abi: abstractedABI,
-  //     functionName: 'safeMint',
-  //     args: [receiver, uri, filename, version, checksum],
-  // })
-  // const {data, isLoading, isSuccess, write: mintWrite}  = useContractWrite(configMinting)
-  console.log("hasdeployed: ",hasDeployed);
-  
+  function getAddressName(address) {
+    for (const key in contractsData) {
+      if (contractsData.hasOwnProperty(key) && contractsData[key].op === address) {
+        return key;
+      }
+    }
+    return "Unknown";
+  }
+
   return (
     <div className="mainstyle-container">
-      
       <h1>Dashboard</h1>
       {isConnecting && <p>Connecting...</p>}
       {isDisconnected && <p>⚠️ You're not connected</p>}
       {!isConnecting && !isDisconnected && (
         <>
-        <div className="mainstyle-card">
-          <p>🎉 Welcome back <b>{userAddress}</b> 🎉</p>
-          <p>Your balance is:<b> {(Number(balance.value)/(10**balance.decimals)).toFixed(3)} ETH</b></p>
-          <p>Here you can manage your abstracted account permissions</p>
-        </div>
-        {hasDeployed && (
           <div className="mainstyle-card">
-            <p>🎉 You have deployed an abstracted account 🎉</p>
-            </div>
-            )}
-        {!hasDeployed && (
-          <div className="mainstyle-card">
-            <p>⚠️ You have not deployed an abstracted account ⚠️</p>
+            <p>🎉 Welcome back <b>{userAddress}</b> 🎉</p>
+            <p>Your balance is: <b>{(Number(balance.value) / (10 ** balance.decimals)).toFixed(3)} ETH</b></p>
+            <p>Here you can manage your abstracted account permissions</p>
           </div>
-        )}
-
+          <div className="mainstyle-card">
+            <h2>Authorized Addresses</h2>
+            {authorizedAddress && authorizedAddress.length > 0 ? (
+              <ul>
+                {authorizedAddress.map((address, index) => (
+                  <li key={index}>
+                    {getAddressName(address)} ({address})
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p>No authorized addresses found.</p>
+            )}
+          </div>
         </>
       )}
     </div>
